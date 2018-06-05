a43148019c1b5a8b017866dbed528110db88cbf3
Franciu <franciuran@gmail.com>
2018-06-05 11:20:49 +0200

    Ho svuotato la home

---
+++ js/carica_annuncio.js
@@ -0,0 +1,22 @@
+document.getElementById("aggiungi").onclick = function () {
+
+    var formData = new FormData($("#form-carica").get(0));
+    var urlUploder = "../../php/uploader.php";
+
+    $.ajax({
+
+        url: urlUploder,
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+
+        success: function (data) {
+
+            alert(data);
+
+        },
+
+
+    });
+}
