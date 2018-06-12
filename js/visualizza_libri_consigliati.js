4a7e4651553f6b17079bbacbf05c1e60f8e67ec3
Andrea <andreacb94>
2018-06-12 21:30:55 +0200

    Fatto:
    1.  apre link libro consigliato su un'altra scheda

--- js/visualizza_libri_consigliati.js
+++ js/visualizza_libri_consigliati.js
@@ -48,7 +48,7 @@ function MyClick(materia) {
                             '                                    <label for="titololibro">Autore:' + ' ' + array[i].autore + '</label>' + '<br>' +
                             '                                    <label for="titololibro">Casa Editrice:' + ' ' + array[i].casa_editrice + '</label>' + '<br>' +
                             '                                    <label for="titololibro">Edizione:' + ' ' + array[i].edizione + '</label>' + '<br>' +
-                            '                                    <a for="titololibro" >Link:' + ' ' + '<a href="' + array[i].link + '">' + array[i].link + '</a></label>' + '<br>' +
+                            '                                    <label for="titololibro" >Link:' + ' ' + '<a target="_blank" href="' + array[i].link + '">' + array[i].link + '</a></label>' + '<br>' +
 
                             '                                </div>' +
                             '                                </div>' +
