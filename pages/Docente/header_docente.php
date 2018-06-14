2b14fc0324df5c1514f52131ec78de1d5c13b231
MariannaPerrotta <mariannaperrotta95@gmail.com>
2018-06-14 22:22:46 +0200

    menu ok

--- pages/Docente/header_docente.php
+++ pages/Docente/header_docente.php
@@ -132,14 +132,7 @@ if (($_SESSION['utente']['tipo'])!= "docente") {
     </div>
 </div>
 
-<!-- Bootstrap core JavaScript-->
-<script src="../../vendor/jquery/jquery.min.js"></script>
-<script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- Core plugin JavaScript-->
 
-<script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
-
-<script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
 
 
 <!-- Custom scripts for this page-->
