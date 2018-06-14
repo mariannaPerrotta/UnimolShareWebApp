1d2b973826f3322cdf82a7de1a43e5dc631938f4
cricciardi <chiara.ricciardi66.cr@gmail.com>
2018-06-15 01:28:26 +0200

    menu

--- pages/Docente/header_docente.php
+++ pages/Docente/header_docente.php
@@ -135,7 +135,17 @@ if (($_SESSION['utente']['tipo'])!= "docente") {
 
 
 
+<script src="../../vendor/chart.js/Chart.min.js"></script>
+<script src="../../vendor/datatables/jquery.dataTables.js"></script>
+<script src="../../vendor/datatables/dataTables.bootstrap4.js"></script>
+<!-- Bootstrap core JavaScript-->
+<!-- Custom scripts for all pages-->
+<script src="../../js/sb-admin.min.js"></script>
 <!-- Custom scripts for this page-->
+<script src="../../js/sb-admin-datatables.min.js"></script>
+<script src="../../js/sb-admin-charts.min.js"></script>
+
+
 <!-- Toggle between fixed and static navbar-->
 <script>
     $('#toggleNavPosition').click(function () {
--- pages/Studente/header.php
+++ pages/Studente/header.php
@@ -152,6 +152,19 @@ if (($_SESSION['utente']['tipo'])!= "studente") {
 
 
 
+  <script src="../../vendor/chart.js/Chart.min.js"></script>
+  <script src="../../vendor/datatables/jquery.dataTables.js"></script>
+  <script src="../../vendor/datatables/dataTables.bootstrap4.js"></script>
+  <!-- Bootstrap core JavaScript-->
+  <!-- Custom scripts for all pages-->
+  <script src="../../js/sb-admin.min.js"></script>
+  <!-- Custom scripts for this page-->
+  <script src="../../js/sb-admin-datatables.min.js"></script>
+  <script src="../../js/sb-admin-charts.min.js"></script>
+
+
+
+
 
 
     <!-- Custom scripts for this page-->
